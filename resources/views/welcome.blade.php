52c5ccf4550a2a0c6bbbf421800ca6c46fd9c7b1
Emily <emily.laffrey@gmail.com>
2025-03-25 12:15:30 -0600

    add globe icon and create desktab navigation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,18 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <nav class="bg-[#0f4e43] text-[#d1fd98] p-4 flex items-center gap-4">
+        <a class="flex items-center gap-2 text-lg" href="">
+            <x-icons.globe-americas class="size-8" />
+            Weather Dashboard
+        </a>
+        <div class="pl-12 space-x-4">
+            <a href="" class="hover:text-white transition-colors">Home</a>
+            <a href="https://google.com" class="hover:text-white transition-colors">Google</a>
+        </div>
+    </nav>
     <h1 class="text-2xl font-bold">Weather Dashboard</h1>
+
     <div>
         <p>Current Temperature: <span>XX</span></p>
         <p>Current Condition: Sunny</p>
