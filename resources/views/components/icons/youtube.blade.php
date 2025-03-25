5f9001f8d2854364db8aac62ec978fe2457a4cc8
Emily <emily.laffrey@gmail.com>
2025-03-25 12:54:15 -0600

    add social icons

---
+++ resources/views/components/icons/youtube.blade.php
@@ -0,0 +1,5 @@
+@props(['class' => 'w-6 h-6'])
+
+<svg xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => $class]) }} viewBox="0 0 48 48" fill="currentColor">
+  <path d="M47.5 14.4c0-4.6-3.7-8.3-8.3-8.3H8.8c-4.6 0-8.3 3.7-8.3 8.3v19.2c0 4.6 3.7 8.3 8.3 8.3h30.4c4.6 0 8.3-3.7 8.3-8.3V14.4zm-22.7 19.1V14.5l14.4 9.5-14.4 9.5z"/>
+</svg>
