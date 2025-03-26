58a6e49036c79b272b92ca52db08cfa020932029
Emily <emily.laffrey@gmail.com>
2025-03-25 21:11:22 -0600

    remove API route registration from boot function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Http\Controllers\WeatherController;
 use App\Services\ApiService;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,10 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Route::middleware('api')
-            ->prefix('api')
-            ->group(function () {
-                Route::get('/weather', [WeatherController::class, 'getForecast']);
-            });
+        //
     }
 }
